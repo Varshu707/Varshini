15abc27a2add7648d38b853907936a623fe605aa
Varshini <varsaap2006@gmail.com>
2025-05-16 16:00:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var a=10;
+var b=20;
+console.log(a)
+console.log(b)
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a>b)
+console.log(a<b)
+console.log(a==b)
+function add(){
+      var a=10;
+      var b=20;
+      
+}
+add()
+function add(a, b){
+      console.log(a+b)
+}
+add(90,89)
+add(34,67)
+Sub()
+function Sub(a, b){
+      console.log(a-b)
+
+}
+Sub(20,80)
